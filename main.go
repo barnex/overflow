2a424323d3d13b707f078d26b6f78bf43b0dbc20
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2013-12-10 13:14:19 +0100

    calc E

--- main.go
+++ main.go
@@ -1,21 +1,22 @@
 package main
 
-import(
+import (
+	"fmt"
 	"math"
 	"os"
-	"fmt"
 )
 
 var (
-	Nx, Ny int         //size
-	cx, cy float64 = 1, 1 // cellsize
-	rho    [][]float64 // charge density
-	Kx, Ky [][]float64 // rho->E kernel
-	Ex, Ey [][]float64 // electric field
-	sx, sy [][]float64 // conductivity
-	jx, jy [][]float64 // current density
-	Bx, By [][]float64 // boundary current density
-	dt float64
+	Nx, Ny   int                //size
+	KNx, KNy int                // kernel size
+	cx, cy   float64     = 1, 1 // cellsize
+	rho      [][]float64        // charge density
+	Kx, Ky   [][]float64        // rho->E kernel
+	Ex, Ey   [][]float64        // electric field
+	sx, sy   [][]float64        // conductivity
+	jx, jy   [][]float64        // current density
+	Bx, By   [][]float64        // boundary current density
+	dt       float64
 )
 
 func main() {
@@ -26,51 +27,97 @@ func main() {
 	initK()
 	save("Kx", Kx)
 	save("Ky", Ky)
+
+	rho[Ny/2][Nx/2] = 1
+	save("rho", rho)	
+
+	updE()
+	save("Ex", Ex)	
+	save("Ey", Ey)	
+
 }
 
-
-func updRho(){
-	for iy := range rho{
-		for ix := range rho[iy]{
+func updRho() {
+	for iy := range rho {
+		for ix := range rho[iy] {
 			rho[iy][ix] += dt * (jx[iy][ix] + jx[iy][ix+1] + jy[iy][ix] + jy[iy+1][ix])
 		}
 	}
 }
 
-func updE(){
-	
+func updE() {
+
+	// Zero output first
+	for iy := range Ex {
+		for ix := range Ex[iy] {
+			Ex[iy][ix] = 0
+		}
+	}
+
+	for iy := range Ey {
+		for ix := range Ey[iy] {
+			Ey[iy][ix] = 0
+		}
+	}
+
+	for sy := range rho {
+		for sx := range rho[sy] {
+
+			if rho[sy][sx] == 0 {
+				continue // skip zero source
+			}
+
+			for y := range Ex {
+				j := wrap(y-sy, KNy)
+				for x := range Ex[y] {
+					i := wrap(x-sx, KNx)
+					Ex[y][x] += rho[sy][sx] * Kx[j][i]
+				}
+			}
+
+			for y := range Ey {
+				j := wrap(y-sy, KNy)
+				for x := range Ey[y] {
+					i := wrap(x-sx, KNx)
+					Ey[y][x] += rho[sy][sx] * Ky[j][i]
+				}
+			}
+
+		}
+	}
+
 }
 
-func initK(){
-	KNx, KNy := 2*Nx, 2*Ny
+func initK() {
+	KNx, KNy = 2*Nx, 2*Ny
 	Kx, Ky = arr2d(KNx, KNy), arr2d(KNx, KNy)
 
-	for x := -Nx+1; x <= Nx; x++{
+	for x := -Nx + 1; x <= Nx; x++ {
 		xw := wrap(x, KNx)
-		for y := -Ny+1; y <= Ny; y++{
+		for y := -Ny + 1; y <= Ny; y++ {
 			yw := wrap(y, KNy)
 
 			{
-			rx, ry := (float64(x) - 0.5) * cx, float64(y) * cy
-			r := math.Sqrt(rx*rx + ry*ry)
-			r3 := r*r*r
-			Ex := rx / r3
-			Kx[yw][xw] = Ex
+				rx, ry := (float64(x)-0.5)*cx, float64(y)*cy
+				r := math.Sqrt(rx*rx + ry*ry)
+				r3 := r * r * r
+				Ex := rx / r3
+				Kx[yw][xw] = Ex
 			}
 
 			{
-			rx, ry := (float64(x)) * cx, (float64(y)-0.5) * cy
-			r := math.Sqrt(rx*rx + ry*ry)
-			r3 := r*r*r
-			Ey := ry / r3
-			Ky[yw][xw] = Ey
+				rx, ry := (float64(x))*cx, (float64(y)-0.5)*cy
+				r := math.Sqrt(rx*rx + ry*ry)
+				r3 := r * r * r
+				Ey := ry / r3
+				Ky[yw][xw] = Ey
 			}
 
 		}
 	}
 }
 
-func alloc(){
+func alloc() {
 	rho = arr2d(Nx, Ny)
 	Ex, Ey = staggered(Nx, Ny)
 	sx, sy = staggered(Nx, Ny)
@@ -78,7 +125,7 @@ func alloc(){
 	Bx, By = staggered(Nx, Ny)
 }
 
-func staggered(Nx, Ny int) (x [][]float64, y[][]float64){
+func staggered(Nx, Ny int) (x [][]float64, y [][]float64) {
 	return arr2d(Nx+1, Ny), arr2d(Nx, Ny+1)
 }
 
@@ -101,29 +148,29 @@ func wrap(number, max int) int {
 		number -= max
 	}
 	return number
-} 
+}
 
-func save(fname string, a[][]float64){
-	f, err := os.OpenFile(fname, os.O_CREATE | os.O_TRUNC | os.O_WRONLY, 0666)
+func save(fname string, a [][]float64) {
+	f, err := os.OpenFile(fname, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0666)
 	check(err)
 	defer f.Close()
 
-	for iy := range a{
-		for ix := range a[iy]{
+	for iy := range a {
+		for ix := range a[iy] {
 			fmt.Fprint(f, a[iy][ix], " ")
 		}
 		fmt.Fprintln(f)
 	}
 }
 
-func check (err error){
-	if err != nil{
+func check(err error) {
+	if err != nil {
 		panic(err)
 	}
 }
 
-func assert(test bool){
-	if !test{
+func assert(test bool) {
+	if !test {
 		panic("assertion failed")
 	}
 }
